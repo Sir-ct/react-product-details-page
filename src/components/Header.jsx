41213c159ca9761148269c1d76d0e6be8298ff7c
sir-ct <davidsirct@gmail.com>
2022-07-18 17:10:17 +0100

    updating logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header(){
     return(
         <div className="nav">
-            <img src="http://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
+            <img src="https://i.dlpng.com/static/png/197684_preview.png" alt="Amazon Logo" />
         </div>
     )
 }
